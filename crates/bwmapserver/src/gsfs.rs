46255af48c49eb925abad4ad2baa4bb8a8d6ce50
zzlk <2418897+zzlk@users.noreply.github.com>
2025-02-20 16:17:13 -0800

    upload chkblobs to gsfs as well as mapblobs

--- crates/bwmapserver/src/gsfs.rs
+++ crates/bwmapserver/src/gsfs.rs
@@ -33,19 +33,17 @@ async fn read_file_as_stream(
     })
 }
 
-pub async fn gsfs_put_mapblob(
+async fn gsfs_put(
     client: &Client,
     endpoint: &str,
-    path: impl AsRef<Path> + 'static,
-    mapblob_hash: &str,
+    src: impl Stream<Item = Result<Bytes, std::io::Error>> + Sync + Send + 'static,
+    dst: impl AsRef<str> + 'static,
 ) -> Result<()> {
+    anyhow::ensure!(dst.as_ref().starts_with("/"), "dst must start with /");
+
     let response = client
-        .put(format!(
-            "{endpoint}/api/fs/scmscx.com/mapblob/{mapblob_hash}"
-        ))
-        .body(Body::wrap_stream(
-            read_file_as_stream(path, 1024 * 1024).await?,
-        ))
+        .put(format!("{endpoint}/api/fs/scmscx.com{}", dst.as_ref()))
+        .body(Body::wrap_stream(src))
         .send()
         .await?;
 
@@ -60,15 +58,15 @@ pub async fn gsfs_put_mapblob(
     Ok(())
 }
 
-pub async fn gsfs_get_mapblob(
+async fn gsfs_get(
     client: &Client,
     endpoint: &str,
-    mapblob_hash: &str,
+    path: impl AsRef<str> + 'static,
 ) -> Result<impl Stream<Item = Result<Bytes, reqwest::Error>>> {
+    anyhow::ensure!(path.as_ref().starts_with("/"), "path must start with /");
+
     let response = client
-        .get(format!(
-            "{endpoint}/api/fs/scmscx.com/mapblob/{mapblob_hash}"
-        ))
+        .get(format!("{endpoint}/api/fs/scmscx.com{}", path.as_ref()))
         .send()
         .await?;
 
@@ -82,3 +80,49 @@ pub async fn gsfs_get_mapblob(
 
     Ok(response.bytes_stream())
 }
+
+pub async fn gsfs_put_mapblob(
+    client: &Client,
+    endpoint: &str,
+    path: impl AsRef<Path> + 'static,
+    mapblob_hash: &str,
+) -> Result<()> {
+    gsfs_put(
+        client,
+        endpoint,
+        read_file_as_stream(path, 1024 * 1024).await?,
+        format!("/mapblob/{mapblob_hash}"),
+    )
+    .await
+}
+
+pub async fn gsfs_get_mapblob(
+    client: &Client,
+    endpoint: &str,
+    mapblob_hash: &str,
+) -> Result<impl Stream<Item = Result<Bytes, reqwest::Error>>> {
+    gsfs_get(client, endpoint, format!("/mapblob/{mapblob_hash}")).await
+}
+
+pub async fn gsfs_put_chkblob(
+    client: &Client,
+    endpoint: &str,
+    path: impl AsRef<Path> + 'static,
+    chkblob_hash: &str,
+) -> Result<()> {
+    gsfs_put(
+        client,
+        endpoint,
+        read_file_as_stream(path, 1024 * 1024).await?,
+        format!("/chkblob/{chkblob_hash}"),
+    )
+    .await
+}
+
+// pub async fn gsfs_get_chkblob(
+//     client: &Client,
+//     endpoint: &str,
+//     chkblob_hash: &str,
+// ) -> Result<impl Stream<Item = Result<Bytes, reqwest::Error>>> {
+//     gsfs_get(client, endpoint, format!("/chkblob/{chkblob_hash}")).await
+// }
